335ed2b98d3769a8982795c0086a63574a643c56
Valeria Oreshko <oreshko-v@yandex.ru>
2023-07-22 18:39:19 +0300

    Fix

--- src/test/java/ru/netology/DebitCardTest.java
+++ src/test/java/ru/netology/DebitCardTest.java
@@ -9,13 +9,51 @@ import static com.codeborne.selenide.Selenide.open;
 
 class DebitCardTest {
     @Test
-    void shouldTest() {
+    void shouldPositiveTest() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Мария Иванова");
+        $("[data-test-id=phone] input").setValue("+79110000000");
+        $(".checkbox").click();
+        $(".button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+    @Test
+    void shouldNegativeTestName() {
         open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Maria Ivanova");
         $("[data-test-id=phone] input").setValue("+79110000000");
         $(".checkbox").click();
         $(".button").click();
-        $("[data-test-id=name]").shouldHave(exactText("Фамилия и имя Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldNegativeTestPhone() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Мария Иванова");
+        $("[data-test-id=phone] input").setValue("+7911000000");
+        $(".checkbox").click();
+        $(".button").click();
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldNegativeTestNameAndPhone() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("");
+        $("[data-test-id=phone] input").setValue("");
+        $(".checkbox").click();
+        $(".button").click();
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldNegativeTestCheckbox() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Мария Иванова");
+        $("[data-test-id=phone] input").setValue("+79110000000");
+        $(".button").click();
+        $(".checkbox").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
     }
 
 }
